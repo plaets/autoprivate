6a6ce8b347dee68d9949cbe3ec1e73942ff922db
plaets <plates.jsnm@gmail.com>
2018-03-23 19:56:08 +0100

    Prototype of extension done, hardcoded only for twitter

--- js/background.js
+++ js/background.js
@@ -1,10 +1,21 @@
+//TODO permissions
 console.log("loaded");
+
+function processTabInfo(object, tabInfo)
+{
+    if(!tabInfo.incognito)
+    {
+        var creating = browser.windows.create({url: object.url, incognito: true});
+        browser.tabs.remove(object.tabId);
+        creating.then((window)=>{console.log("created incognito window with id " + window.id)}, (error)=>{console.log(error)});
+    }
+}
 
 function navigationListener(object)
 {
-    console.log("event: " + object.url);
+    var tabsPromise = browser.tabs.get(object.tabId);
+    tabsPromise.then(processTabInfo.bind(null, object), (error)=>{console.log(error)});
 }
 
-
-var filter = {url:[{hostContains: "google.com"}]};
-browser.webNavigation.onBeforeNavigate.addListener(navigationListener, filter);
+var filter = {hostContains: "twitter"};
+browser.webNavigation.onBeforeNavigate.addListener(navigationListener, {url:[filter]});
